eb53659a2df7e9c3f7d7b030acd4148eab426381
Julien MIQUEL <julienmiquel@google.com>
2023-12-20 09:53:09 +0100

    add output cloud storage folder in function variable

--- cloud_function_trigger/main.py
+++ cloud_function_trigger/main.py
@@ -1,11 +1,12 @@
 import time
- 
+import os
+
 from google.cloud import videointelligence
  
 video_client = videointelligence.VideoIntelligenceServiceClient()
  
-OUTPUT_BUCKET = "gs://YOUR OUTPUT BUCKET NAME"
- 
+OUTPUT_BUCKET  = os.environ.get("OUTPUT_BUCKET")
+
 features = [
     videointelligence.Feature.OBJECT_TRACKING,
     videointelligence.Feature.LABEL_DETECTION,
@@ -47,7 +48,58 @@ def analyze_video(event, context):
     input_uri = "gs://" + event["bucket"] + "/" + event["name"]
     file_stem = event["name"].split(".")[0]
     output_uri = f"{OUTPUT_BUCKET}/{file_stem} - {time.time()}.json"
- 
+    
+    print(f"input_uri = {input_uri} - output_uri = {output_uri} file_stem = {file_stem}")
+    language_code="en-US"
+    #if the input_uri contain "en-US" set the language_code variable to "en-US"
+    if "en-US" in input_uri:
+        language_code="en-US"
+        
+    if "fr-FR" in input_uri:        
+        language_code="fr-FR"
+        
+    if "es-ES" in input_uri:        
+        language_code="es-ES"   
+        
+    if "de-DE" in input_uri:        
+        language_code="de-DE"
+
+    if "it-IT" in input_uri:        
+        language_code="it-IT"
+
+    if "pt-PT" in input_uri:        
+        language_code="pt-PT"
+
+    if "pt-BR" in input_uri:        
+        language_code="pt-BR"
+
+    if "zh-CN" in input_uri:        
+        language_code="zh-CN"
+
+    if "ja-JP" in input_uri:        
+        language_code="ja-JP"
+
+    if "ko-KR" in input_uri:        
+        language_code="ko-KR"
+
+    if "ar-XA" in input_uri:        
+        language_code="ar-XA"
+
+    if "ru-RU" in input_uri:        
+        language_code="ru-RU"
+
+    if "hi-IN" in input_uri:        
+        language_code="hi-IN"
+        
+    speech_config = videointelligence.SpeechTranscriptionConfig(
+        language_code=language_code,
+        enable_automatic_punctuation=True,
+    )
+    video_context = videointelligence.VideoContext(
+        speech_transcription_config=speech_config,
+        person_detection_config=person_config,
+        face_detection_config=face_config,
+    )
     video_client.annotate_video(
         request={
             "features": features,

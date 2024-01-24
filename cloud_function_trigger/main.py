2f293df91f5f57e7e30fb6aefd23c9620889adf0
Julien Miquel <86713160+julienmiquel@users.noreply.github.com>
2024-01-24 20:04:05 +0000

    try to solve

--- cloud_function_trigger/main.py
+++ cloud_function_trigger/main.py
@@ -1,50 +1,50 @@
 import time
 import os
 
-from google.cloud import videointelligence
- 
-video_client = videointelligence.VideoIntelligenceServiceClient()
- 
-OUTPUT_BUCKET  = os.environ.get("OUTPUT_BUCKET")
-
-features = [
-    videointelligence.Feature.OBJECT_TRACKING,
-    videointelligence.Feature.LABEL_DETECTION,
-    videointelligence.Feature.SHOT_CHANGE_DETECTION,
-    videointelligence.Feature.SPEECH_TRANSCRIPTION,
-    videointelligence.Feature.LOGO_RECOGNITION,
-    videointelligence.Feature.EXPLICIT_CONTENT_DETECTION,
-    videointelligence.Feature.TEXT_DETECTION,
-    videointelligence.Feature.FACE_DETECTION,
-    videointelligence.Feature.PERSON_DETECTION,
-]
- 
-speech_config = videointelligence.SpeechTranscriptionConfig(
-    language_code="en-US",
-    enable_automatic_punctuation=True,
-)
- 
-person_config = videointelligence.PersonDetectionConfig(
-    include_bounding_boxes=True,
-    include_attributes=False,
-    include_pose_landmarks=True,
-)
- 
-face_config = videointelligence.FaceDetectionConfig(
-    include_bounding_boxes=True,
-    include_attributes=True,
-)
- 
-video_context = videointelligence.VideoContext(
-    speech_transcription_config=speech_config,
-    person_detection_config=person_config,
-    face_detection_config=face_config,
-)
- 
  
 def analyze_video(event, context):
     print(event)
 
+    from google.cloud import videointelligence
+    
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    
+    OUTPUT_BUCKET  = os.environ.get("OUTPUT_BUCKET")
+
+    features = [
+        videointelligence.Feature.OBJECT_TRACKING,
+        videointelligence.Feature.LABEL_DETECTION,
+        videointelligence.Feature.SHOT_CHANGE_DETECTION,
+        videointelligence.Feature.SPEECH_TRANSCRIPTION,
+        videointelligence.Feature.LOGO_RECOGNITION,
+        videointelligence.Feature.EXPLICIT_CONTENT_DETECTION,
+        videointelligence.Feature.TEXT_DETECTION,
+        videointelligence.Feature.FACE_DETECTION,
+        videointelligence.Feature.PERSON_DETECTION,
+    ]
+    
+    speech_config = videointelligence.SpeechTranscriptionConfig(
+        language_code="en-US",
+        enable_automatic_punctuation=True,
+    )
+    
+    person_config = videointelligence.PersonDetectionConfig(
+        include_bounding_boxes=True,
+        include_attributes=False,
+        include_pose_landmarks=True,
+    )
+    
+    face_config = videointelligence.FaceDetectionConfig(
+        include_bounding_boxes=True,
+        include_attributes=True,
+    )
+    
+    video_context = videointelligence.VideoContext(
+        speech_transcription_config=speech_config,
+        person_detection_config=person_config,
+        face_detection_config=face_config,
+    )
+    
  
     input_uri = "gs://" + event["bucket"] + "/" + event["name"]
     file_stem = event["name"].split(".")[0]
